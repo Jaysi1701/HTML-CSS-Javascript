5ce5c0f88add9427ce38a151c8021dec2628d109
jaysi1701 <jaysigaur9449@gmail.com>
2026-06-11 13:01:39 +0530

    Added UC2 Email Validation

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
+// UC1 - Name Validation
+
 function validateName() {
 
-    const name = document.getElementById("name").value;
+    const name =
+        document.getElementById("name").value;
 
     const error =
         document.getElementById("nameError");
@@ -8,11 +11,34 @@ function validateName() {
     const nameRegex =
         /^[A-Z][a-zA-Z]{2,}$/;
 
-    if (nameRegex.test(name)) {
+    if(nameRegex.test(name)){
         error.textContent = "";
     }
-    else {
+    else{
         error.textContent =
-            "Name should start with a Capital Letter and have minimum 3 characters";
+        "Name should start with Capital Letter and contain minimum 3 characters";
+    }
+}
+
+
+// UC2 - Email Validation
+
+function validateEmail(){
+
+    const email =
+        document.getElementById("email").value;
+
+    const error =
+        document.getElementById("emailError");
+
+    const emailRegex =
+        /^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/;
+
+    if(emailRegex.test(email)){
+        error.textContent = "";
+    }
+    else{
+        error.textContent =
+        "Enter a valid email address";
     }
 }
