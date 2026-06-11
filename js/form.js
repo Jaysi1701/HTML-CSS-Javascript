1244ac68ddccd7cb9c4663b79e13ed523ecb85a5
jaysi1701 <jaysigaur9449@gmail.com>
2026-06-11 13:04:53 +0530

    Added UC4 Password Rule1 Validation

--- js/form.js
+++ js/form.js
@@ -1,67 +1,68 @@
-// UC1 - Name Validation
+// UC1
 
 function validateName() {
 
-    const name =
-        document.getElementById("name").value;
+    const name = document.getElementById("name").value;
+    const error = document.getElementById("nameError");
 
-    const error =
-        document.getElementById("nameError");
+    const nameRegex = /^[A-Z][a-zA-Z]{2,}$/;
 
-    const nameRegex =
-        /^[A-Z][a-zA-Z]{2,}$/;
-
-    if(nameRegex.test(name)){
-        error.textContent = "";
-    }
-    else{
-        error.textContent =
-        "Name should start with Capital Letter and contain minimum 3 characters";
-    }
+    error.textContent =
+        nameRegex.test(name)
+        ? ""
+        : "Name should start with Capital Letter and contain minimum 3 characters";
 }
 
 
-// UC2 - Email Validation
+// UC2
 
-function validateEmail(){
+function validateEmail() {
 
-    const email =
-        document.getElementById("email").value;
-
-    const error =
-        document.getElementById("emailError");
+    const email = document.getElementById("email").value;
+    const error = document.getElementById("emailError");
 
     const emailRegex =
         /^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/;
 
-    if(emailRegex.test(email)){
-        error.textContent = "";
-    }
-    else{
-        error.textContent =
-        "Enter a valid email address";
-    }
+    error.textContent =
+        emailRegex.test(email)
+        ? ""
+        : "Enter a valid email address";
 }
 
 
-// UC3 - Mobile Validation
+// UC3
 
-function validateMobile(){
+function validateMobile() {
 
-    const mobile =
-        document.getElementById("mobile").value;
-
-    const error =
-        document.getElementById("mobileError");
+    const mobile = document.getElementById("mobile").value;
+    const error = document.getElementById("mobileError");
 
     const mobileRegex =
         /^[0-9]{2}\s[0-9]{10}$/;
 
-    if(mobileRegex.test(mobile)){
-        error.textContent = "";
-    }
-    else{
-        error.textContent =
-        "Format should be 91 9876543210";
-    }
+    error.textContent =
+        mobileRegex.test(mobile)
+        ? ""
+        : "Format should be 91 9876543210";
+}
+
+
+// UC4
+
+function validatePassword() {
+
+    const password =
+        document.getElementById("password").value;
+
+    const error =
+        document.getElementById("passwordError");
+
+    const passwordRegex =
+        /^.{8,}$/;
+
+    error.textContent =
+        passwordRegex.test(password)
+        ? ""
+        : "Password must contain minimum 8 characters";
 }
